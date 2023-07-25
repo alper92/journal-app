054220f9547623b6090b0c33c65469b422ca5452
Alper Kürtül <alperkuertuel@hotmail.de>
2023-07-25 21:39:19 +0200

    add entry after form submit to entry section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
+import { initialEntries } from "./data/initialEntries"; // initial data for the entries to map them
+import { useState } from "react";
+import { uid } from "uid";
+
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  function handleAddEntry(newEntry) {
+    const date = new Date().toLocaleDateString("en-us", {
+      dateStyle: "medium",
+    });
+    setEntries([{ id: uid(), date, ...newEntry }, ...entries]);
+  }
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
+        <EntryForm onAddEntry={handleAddEntry} />
+        <EntriesSection entries={entries} />
       </main>
       <Footer />
     </div>
