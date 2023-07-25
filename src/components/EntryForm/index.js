6d44bf8a1f85d9d7df56d2d5e93935cf8c9b79a3
Alper Kürtül <alperkuertuel@hotmail.de>
2023-07-25 21:54:50 +0200

    add required inputs to avoid empty entries

--- src/components/EntryForm/index.js
+++ src/components/EntryForm/index.js
@@ -5,13 +5,11 @@ export default function EntryForm({ onAddEntry }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(event.target.motto.value);
-
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    onAddEntry(data);
+
     console.log(data);
-    // event.target.reset();
+    event.target.reset();
   }
 
   return (
@@ -20,11 +18,11 @@ export default function EntryForm({ onAddEntry }) {
       <div className="entry-form__fields">
         <div className="entry-form__field">
           <label htmlFor="motto">Motto</label>
-          <input type="text" name="motto" id="motto" />
+          <input type="text" name="motto" id="motto" required />
         </div>
         <div className="entry-form__field">
           <label htmlFor="notes">Notes</label>
-          <textarea name="notes" id="notes" rows="4" />
+          <textarea name="notes" id="notes" rows="4" required />
         </div>
         <div className="entry-form__button-wrapper">
           <Button type="submit">Create</Button>
