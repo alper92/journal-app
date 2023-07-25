251ef7364057e9b7523defce899a1fdce195db50
Alper Kürtül <alperkuertuel@hotmail.de>
2023-07-25 09:40:25 +0200

    add comment

--- src/components/FavoriteButton/index.js
+++ src/components/FavoriteButton/index.js
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import "./FavoriteButton.css";
-import { ReactComponent as StarFilled } from "./star-filled.svg";
+import { ReactComponent as StarFilled } from "./star-filled.svg"; // make image immediately a react component
 import { ReactComponent as Star } from "./star.svg";
 
 export default function FavoriteButton() {
-  // this should be a state variable
-
-  const [isFav, setFav] = useState(false);
+  const [isFav, setFav] = useState(false); // initially set "isFav" to "false"
 
   return (
+    // From this point JSX beginns and before the return everythin is javascript
     <button
       className="favorite-button"
       onClick={() => {
         setFav(!isFav);
-        // console.log("favorite button clicked");
+        /* with the click event on the button we change the state of isFav to true!
+        So all the functions before the return will be triggered from top to bottom automatically again!
+        */
       }}
       aria-label="favorite"
     >
-      {isFav ? <StarFilled /> : <Star />}
+      {isFav ? <StarFilled /> : <Star /> /* If "isFave" = true... */}
     </button>
   );
 }
